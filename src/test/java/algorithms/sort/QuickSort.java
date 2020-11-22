08396e875e95b8a1c170b9831d1c3f0929bd0c60
z-dk <zhudengkui0000@126.com>
2020-11-22 18:08:33 +0800

    归并排序，快速排序

---
+++ src/test/java/algorithms/sort/QuickSort.java
@@ -0,0 +1,59 @@
+package algorithms.sort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * <b>类 名 称</b> :  QuickSort<br/>
+ * <b>类 描 述</b> :  快速排序代码实现<br/>
+ * <b>创 建 人</b> :  zhudengkui<br/>
+ * <b>创建时间</b> :  2020/11/22 10:58<br/>
+ * <b>修 改 人</b> :  zhudengkui<br/>
+ * <b>修改时间</b> :  2020/11/22 10:58<br/>
+ * <b>修改备注</b> :
+ */
+public class QuickSort {
+    
+    public static void main(String[] args) {
+        int[] a = {1,7,13,5,18,3,9,6,15};
+        System.out.println("排序前："+Arrays.toString(a));
+        sort(a,0,a.length-1);
+        System.out.println("排序后："+Arrays.toString(a));
+    }
+    
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = partition(a,lo,hi);
+        sort(a,lo,mid-1);
+        sort(a,mid+1,hi);
+    }
+    
+    private static int partition(int[] a, int lo, int hi) {
+        // 将数组切分为a[lo..i-1],a[i],a[i+1..hi]
+        int mid = a[lo], left = lo, right = hi+1;
+        while (true) {
+            while (a[++left] < mid) {
+                if (left == hi) {
+                    break;
+                }
+            }
+            while (a[--right] > mid) {
+                if (right == lo) {
+                    break;
+                }
+            }
+            if (left >= right) break;
+            int swap = a[left];
+            a[left] = a[right];
+            a[right] = swap;
+        }
+        int swap = a[right];
+        a[right] = a[lo];
+        a[lo] = swap;
+        return right;
+    }
+    
+}
