83666d8fd990dffc4e22392e59eddddc7a8e0f29
zdk <zhudengkui0000@126.com>
2022-02-13 09:30:16 +0800

    mybatis-plus代码生成工具调整

--- hello-web/src/main/java/com/zdk/hello/CodeGenerator.java
+++ hello-web/src/main/java/com/zdk/hello/CodeGenerator.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -24,30 +23,32 @@ import java.util.Scanner;
 public class CodeGenerator {
 
     public static void main(String[] args) throws IOException {
-        generatorByTableName("province","province");
+        generatorByTableName("customer", "com.zdk.hello.service", "customer", "");
     }
     
-    public static void generatorByTableName(String tableName,String packageName, String ... filterTablePrefix) {
-        String projectPath = System.getProperty("user.dir") + "\\hello-web\\src\\main\\java";
+    public static void generatorByTableName(String tableName, String parentPackageName, 
+                                            String packageName, String ... filterTablePrefix) {
+        String classPath = System.getProperty("user.dir") + "\\hello-web\\src\\main\\java";
+        String resourcePath = System.getProperty("user.dir") + "\\hello-web\\src\\main\\resources";
         
         FastAutoGenerator.create("jdbc:mysql://localhost:3306/hello-world?useSSL=false", "root", "Mysql@1111")
                 .globalConfig(builder -> {
                     builder.author("zdk") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir(projectPath + "todo"); // 指定输出目录
+                            .outputDir(classPath); // 指定输出目录
                 })
                 .packageConfig(builder -> {
-                    builder.parent(packageName) // 设置父包名
-                            .moduleName("system") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, projectPath + "todo")); // 设置mapperXml生成路径
+                    builder.parent(parentPackageName) // 设置父包名
+                            .moduleName(packageName) // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, resourcePath + "\\config\\mapper"));// 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.addInclude(tableName) // 设置需要生成的表名
                             .addTablePrefix(filterTablePrefix); // 设置过滤表前缀
                 })
                 // 使用Freemarker引擎模板，默认的是Velocity引擎模板
-                .templateEngine(new FreemarkerTemplateEngine()) 
+                //.templateEngine(new FreemarkerTemplateEngine()) 
                 .execute();
     }
     
