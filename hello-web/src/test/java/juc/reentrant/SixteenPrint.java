326d115a7ca49f0c9d0ca6b0ad61cb619a78c2ca
zdk <zhudengkui0000@126.com>
2022-07-30 16:01:54 +0800

    ReentrantLock按数字升序打印

--- hello-web/src/test/java/juc/reentrant/SixteenPrint.java
+++ hello-web/src/test/java/juc/reentrant/SixteenPrint.java
@@ -1,5 +1,8 @@
 package juc.reentrant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,72 +17,82 @@ import java.util.concurrent.locks.ReentrantLock;
  * <b>修 改 人</b> :  zhudengkui<br/>
  * <b>修改时间</b> :  2022/7/30 10:58<br/>
  * <b>修改备注</b> :  <br/>
- *
+ * 多线程按升序打印数字=ReentrantLock实现方式
  * @author zdk
  */
 public class SixteenPrint {
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(SixteenPrint.class);
+    
     static ReentrantLock lock = new ReentrantLock(false);
-    static Condition condition0 = lock.newCondition();
-    static Condition condition1 = lock.newCondition();
-    static Condition condition2 = lock.newCondition();
-    static Condition condition3 = lock.newCondition();
-    static volatile boolean flag = false;
+    static Condition condition = lock.newCondition();
+    static volatile int intFlag = 0;
     
     public static void print(int n) {
+        lock.lock();
         switch (n) {
             case 0:
-                lock.lock();
+                while (intFlag != 0) {
+                    try {
+                        LOGGER.info("0 is waiting");
+                        condition.await();
+                        LOGGER.info("0 is continue");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
                 System.out.println(n);
-                condition1.signal();
-                flag = true;
-                lock.unlock();
+                condition.signalAll();
+                intFlag = 1;
                 break;
             case 1:
-                lock.lock();
-                try {
-                    while (!flag) {
-                        condition1.await();
+                while (intFlag != 1) {
+                    try {
+                        LOGGER.info("1 is waiting");
+                        condition.await();
+                        LOGGER.info("1 is continue");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
                 System.out.println(n);
-                condition2.signal();
-                lock.unlock();
+                condition.signalAll();
+                intFlag = 2;
                 break;
             case 2:
-                lock.lock();
-                try {
-                    while (!flag) {
-                        condition2.await();
+                while (intFlag != 2) {
+                    try {
+                        LOGGER.info("2 is waiting");
+                        condition.await();
+                        LOGGER.info("2 is continue");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
                 System.out.println(n);
-                condition3.signal();
-                lock.unlock();
+                condition.signalAll();
+                intFlag = 3;
                 break;
             case 3:
-                lock.lock();
-                try {
-                    while (!flag) {
-                        condition3.await();
+                while (intFlag != 3) {
+                    try {
+                        LOGGER.info("3 is waiting");
+                        condition.await();
+                        LOGGER.info("3 is continue");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
                 System.out.println(n);
-                condition0.signal();
-                lock.unlock();
+                condition.signalAll();
+                intFlag = 0;
                 break;
         }
         lock.unlock();
     }
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
         AtomicInteger integer = new AtomicInteger(3);
         for (int i = 0; i < 4; i++) {
             executorService.submit(() -> {
