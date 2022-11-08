e4b23ece023e6e3d74cf9dcad826625f74a3ec73
zdk <zhudengkui@idstaff.com>
2022-11-08 17:42:12 +0800

    java基础-剪贴板操作

---
+++ hello-web/src/test/java/base/Clipboard.java
@@ -0,0 +1,24 @@
+package base;
+
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class Clipboard {
+
+    public static void main(String[] args) throws IOException, UnsupportedFlavorException {
+        java.awt.datatransfer.Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        //ClipboardService clipboardService = ClipboardServiceImpl.getInstance();
+        Transferable contents = systemClipboard.getContents(null);
+        if (contents != null && contents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            System.out.println(contents.getTransferData(DataFlavor.stringFlavor));
+        }
+        StringSelection selection = new StringSelection("idea copy!!!");
+        systemClipboard.setContents(selection, selection);
+    }
+
+}
